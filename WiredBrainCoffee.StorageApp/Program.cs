cfe3747cbd6e8a78728022d8faade3168846159c
yuosif <yuosifraed@gmail.com>
2023-08-31 23:25:13 -0700

    edit

--- WiredBrainCoffee.StorageApp/Program.cs
+++ WiredBrainCoffee.StorageApp/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using WiredBrainCoffee.StorageApp.Entities;
+using WiredBrainCoffee.StorageApp.RepositryManagment;
+
+var employeeRepo = new EmployeeRepositry();
+
+employeeRepo.Add(new Employee { FirstName = "yoyo" });
+
+employeeRepo.Add(new Employee { FirstName = "ali" });
+employeeRepo.Add(new Employee { FirstName = "moshtack" });
+
+Console.ReadLine();
