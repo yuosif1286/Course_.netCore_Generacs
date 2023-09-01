036a44c518f054e6a65fdd60086f076674b362b5
yuosif <yuosifraed@gmail.com>
2023-09-01 04:01:44 +0300

    create

--- WiredBrainCoffee.StackApp/Program.cs
+++ WiredBrainCoffee.StackApp/Program.cs
@@ -1,2 +1,40 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using System.Threading.Channels;
+
+var stack = new simpleStack<double>();
+
+stack.Push(1.2);
+stack.Push(2.2);
+stack.Push(4.0);
+
+double sum = 0.0;
+while (stack.Count>0)
+{
+    sum += stack.Pop();
+    Console.WriteLine($"item index:{stack.Count} => Value:{stack.Pop}");
+}
+
+Console.WriteLine($"sum item in stack : {sum}");
+
+
+
+Console.WriteLine("this for type int");
+var stackint = new simpleStack<int>();
+
+stackint.Push(1);
+stackint.Push(2);
+stackint.Push(4);
+
+ sum = 0.0;
+while (stackint.Count > 0)
+{
+    sum += stackint.Pop();
+    Console.WriteLine($"item index:{stackint.Count} => Value:{stackint.Pop}");
+}
+
+Console.WriteLine($"sum item in stack : {sum}");
+
+Console.ReadLine();
+
+
---
+++ WiredBrainCoffee.StackApp/simpleStack.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+public class simpleStack <T>
+{
+    private readonly T[] _item;
+    private int CurrentIndex = -1;
+
+    public simpleStack() => _item = new T[10];
+
+    public void Push(T item) => _item[++CurrentIndex] = item;
+
+    public T Pop() => _item[CurrentIndex--];
+
+    public int Count => CurrentIndex + 1;
+
+}
