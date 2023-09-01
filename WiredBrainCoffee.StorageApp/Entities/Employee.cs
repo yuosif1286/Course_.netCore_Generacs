2165b7d5bfa390fb328688ad8cd565f29db14f8d
yuosif <yuosifraed@gmail.com>
2023-08-31 23:12:36 -0700

    add employee entity

---
+++ WiredBrainCoffee.StorageApp/Entities/Employee.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Employee 
+{
+    public  int id { get; set; }
+    public  string FirstName { get; set; }
+
+    public override string ToString() => $"Id:{id},firstname:{FirstName} ";
+}
