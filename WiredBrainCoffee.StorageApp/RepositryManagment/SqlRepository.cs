7f6e5d0e0c1106d39008e1e0d9e036d3c8a2eb6d
yuosif <yuosifraed@gmail.com>
2023-09-03 03:37:17 +0300

    repo for sql In memory db

---
+++ WiredBrainCoffee.StorageApp/RepositryManagment/SqlRepository.cs
@@ -0,0 +1,27 @@
+using Microsoft.EntityFrameworkCore;
+using WiredBrainCoffee.StorageApp.Entities;
+
+namespace WiredBrainCoffee.StorageApp.RepositryManagment;
+
+public class SqlRepository<T> where T : class, IEntityBase<int>
+{
+    private readonly DbContext _dbContext;
+    private readonly DbSet<T> _dbSet;
+
+    public SqlRepository(DbContext dbContext)
+    {
+        _dbContext = dbContext;
+        _dbSet = _dbContext.Set<T>();
+    }
+
+    public  T? GetById(int id) => _dbSet.Find(id);
+
+
+    public void Add(T item) => _dbSet.Add(item);
+
+    public void Remove(T item) => _dbSet.Remove(item);
+    public void Save()
+    {
+        _dbContext.SaveChanges();
+    }
+}
