6c5ce5cccb74753380522646e51fae1b5cfffd1f
yuosif <yuosifraed@gmail.com>
2023-09-03 03:36:54 +0300

    Add context in memory data base

---
+++ WiredBrainCoffee.StorageApp/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using WiredBrainCoffee.StorageApp.Entities;
+
+namespace WiredBrainCoffee.StorageApp.Data;
+
+public class AppDbContext:DbContext
+{
+    public DbSet<Employee> Employees => Set<Employee>();
+    public DbSet<Orginization>Organizations  => Set<Orginization>();
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+        optionsBuilder.UseInMemoryDatabase("AppCoffeeDb");
+    }
+}
