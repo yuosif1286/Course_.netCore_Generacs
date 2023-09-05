9aa188ec7c1ed5abdd615eacfe09e1263cf62f92
yuosif <yuosifraed@gmail.com>
2023-09-06 01:29:08 +0300

    create generic method

---
+++ WiredBrainCoffee.StorageApp/RepositryManagment/SqlRepository.cs
@@ -0,0 +1,34 @@
+using Microsoft.EntityFrameworkCore;
+using WiredBrainCoffee.StorageApp.Entities;
+
+namespace WiredBrainCoffee.StorageApp.RepositryManagment;
+
+
+public class SqlRepository<T> : IRepository<T>
+    where T : class, IEntityBase
+{
+    private readonly DbContext _dbContext;
+    private readonly DbSet<T> _dbSet;
+
+    public SqlRepository(DbContext dbContext)
+    {
+        _dbContext = dbContext;
+        _dbSet = _dbContext.Set<T>();
+    }
+
+    public T? GetById(int id) => _dbSet.Find(id);
+
+
+    public void Add(T item) => _dbSet.Add(item);
+
+    public void Remove(T item) => _dbSet.Remove(item);
+    public void Save()
+    {
+        _dbContext.SaveChanges();
+    }
+
+    public  IEnumerable<T> GetAll()
+    {
+        return  _dbSet.ToList();
+    }
+}
